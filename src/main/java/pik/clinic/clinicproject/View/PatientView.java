6fc886d6a1f7ab71f1aa0095a46927c06b345bb6
exzhibit97 <exzhibit97@gmail.com>
2019-01-10 10:53:37 +0100

    update patientview

--- src/main/java/pik/clinic/clinicproject/View/PatientView.java
+++ src/main/java/pik/clinic/clinicproject/View/PatientView.java
@@ -1,16 +1,32 @@
 package pik.clinic.clinicproject.View;
 
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.polymertemplate.EventHandler;
 import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
-import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import pik.clinic.clinicproject.Model.Doctor;
+import pik.clinic.clinicproject.Model.Patient;
+import pik.clinic.clinicproject.Model.Visit;
+import pik.clinic.clinicproject.Repositories.DoctorRepository;
+import pik.clinic.clinicproject.Repositories.PatientRepository;
+import pik.clinic.clinicproject.Repositories.VisitRepository;
+
+import javax.annotation.PostConstruct;
 
 /**
  * A Designer generated component for the patient-view.html template.
- *
+ * <p>
  * Designer will add and remove fields with @Id mappings but
  * does not overwrite or otherwise change this file.
  */
@@ -19,14 +35,70 @@ import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 @HtmlImport("patient-view.html")
 public class PatientView extends PolymerTemplate<PatientView.PatientViewModel> {
 
+    TemplateRenderer<Patient> renderer = TemplateRenderer.<Patient>of("");
+    TemplateRenderer<Doctor> rendererDoc = TemplateRenderer.<Doctor>of("");
+    TemplateRenderer<Visit> rendererVisit = TemplateRenderer.<Visit>of("");
+
+    @Autowired
+    VisitRepository visitRepository;
+    @Autowired
+    PatientRepository patientRepository;
+    @Autowired
+    DoctorRepository doctorRepository;
+
     @Id("nameLabel")
     private Label nameLabel;
+    @Id("gridinfo")
+    private Grid<Visit> gridinfo;
+    @Id("patients")
+    private ComboBox<Patient> patients;
+    @Id("doctors")
+    private ComboBox<Doctor> doctors;
+    @Id("visitDate")
+    private DatePicker visitDate;
+    @Id("summary")
+    private TextField summary;
+    @Id("peselLabel")
+    private Label peselLabel;
+    @Id("addresLabel")
+    private Label addresLabel;
 
     /**
      * Creates a new PatientView.
      */
     public PatientView() {
         // You can initialise any data required for the connected UI components here.
+    }
+
+    @PostConstruct
+    public void combo() {
+        patients.setItemLabelGenerator(Patient::getFirstName);
+        patients.setItems(patientRepository.findAll());
+        patients.setRenderer(renderer.withProperty("patientName", Patient::toString));
+        doctors.setItemLabelGenerator(Doctor::getFirstName);
+        doctors.setItems(doctorRepository.findAll());
+        doctors.setRenderer(rendererDoc.withProperty("doctorName", Doctor::toString));
+        //vaadinButton.setEnabled(false);
+    }
+
+
+    @EventHandler
+    public void saveVisit() {
+        Notification.show("TEKST");
+        Visit v = new Visit();
+        v.setPatient(patients.getValue());
+        v.setDoctor(doctors.getValue()); //combobox doktora
+        v.setDateOfVisit(visitDate.getValue()); //wybor daty wizyty
+        v.setSummary(summary.getValue()); //pole na informacje dodatkowa
+        try {
+            if (doctors.getValue() != null && patients.getValue() != null && visitDate.getValue() != null) {
+                visitRepository.save(v);
+                gridinfo.setItems(visitRepository.findAll());
+            }
+        } catch (Exception e) {
+            Notification.show("Wypełnij wszystkie pola!", 5000, Notification.Position.MIDDLE);
+        }
+
     }
 
     /**
