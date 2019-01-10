fc0cf7dc0b717cea800cdf4a3aa1c4baba0fe5fc
exzhibit97 <exzhibit97@gmail.com>
2019-01-10 15:05:21 +0100

    updated

--- src/main/java/pik/clinic/clinicproject/Repositories/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package pik.clinic.clinicproject.Repositories;
-
-import pik.clinic.clinicproject.Model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- src/main/java/pik/clinic/clinicproject/Repositories/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package pik.clinic.clinicproject.Repositories;
-
-import pik.clinic.clinicproject.Model.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
